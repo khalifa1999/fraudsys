703721a784d4c2d8ee25f7d97ed99ddaac31db36
Khalifa Mamadou NIAMADIO <khalifaniamadio@gmail.com>
2022-08-28 22:20:25 +0000

    page configuration

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Automation Application")
 st.subheader("Welcome to dfc fraud automation app")
-st.set_page_config( page_title="dfcapp",
-                    page_icon=None, layout="centered", initial_sidebar_state="auto"
+st.set_page_config(page_title="dfcapp",
+                   page_icon=None, layout="centered", initial_sidebar_state="auto"
 
-)
+                   )
