73c742ef20cd38cc2d547befa79d888d648b30c2
Khalifa Mamadou NIAMADIO <khalifaniamadio@gmail.com>
2022-08-28 21:44:54 +0000

    test

--- it.py
+++ it.py
@@ -1,21 +1,12 @@
 import streamlit as st
 import pandas as pd
-from io import StringIO, BytesIO  # Standard Python Module
-import base64
+
 st.title("Automation Application")
 st.subheader("IT traitement")
 
 uploaded_f = st.sidebar.file_uploader("Choose xlsx file", type=['xlsx', 'xlsb'])
 
 
-def generate_excel_download_link(df):
-    # Credit Excel: https://discuss.streamlit.io/t/how-to-add-a-download-excel-csv-function-to-a-button/4474/5
-    towrite = BytesIO()
-    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
-    towrite.seek(0)  # reset pointer
-    b64 = base64.b64encode(towrite.read()).decode()
-    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
-    return st.markdown(href, unsafe_allow_html=True)
 
 
 # Indicatifs des pays
@@ -102,4 +93,3 @@ if uploaded_f is not None:
     # for it
     unique_val = len(hvi_dataframe.index.unique())
     st.write(unique_val)
-    generate_excel_download_link(hvi_dataframe)
