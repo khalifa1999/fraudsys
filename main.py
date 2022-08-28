6613a6d5da76a81449936f16c3e78da07378026b
Khalifa Mamadou NIAMADIO <khalifaniamadio@gmail.com>
2022-08-28 22:15:13 +0000

    adding some subpages

--- main.py
+++ main.py
@@ -1,42 +1,8 @@
 import streamlit as st
-import pandas as pd
 
 st.title("Automation Application")
-st.subheader("IT traitement")
+st.subheader("Welcome to dfc fraud automation app")
 st.set_page_config( page_title="dfcapp",
                     page_icon=None, layout="centered", initial_sidebar_state="auto"
 
 )
-uploaded_f = st.sidebar.file_uploader("Choose xlsx file", type=['xlsx', 'xlsb'])
-
-
-# For IT
-@st.experimental_memo
-def it():
-    # color function
-    def colour(value):
-        if value > 80:
-            color = 'red'
-        else:
-            color = 'green'
-        return 'color: %s' % color
-
-    df = pd.read_excel(uploaded_f, engine='openpyxl')
-
-    it_df = df.groupby('num_ligne')['nb_appels', 'heure'].sum()
-    it_df['outils'] = "IT"
-    it_df = it_df.sort_values(
-        by='nb_appels',
-        ascending=False
-    )
-    it_df = it_df.style.applymap(colour, subset=['nb_appels'])
-    return it_df
-
-
-if uploaded_f is not None:
-    hvi_dataframe = it()
-    st.dataframe(hvi_dataframe)
-
-    # for it
-    unique_val = len(hvi_dataframe.index.unique())
-    st.write(unique_val)
---
+++ pages/it.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Automation Application")
+st.subheader("IT traitement")
+
+
+uploaded_f = st.sidebar.file_uploader("Choose xlsx file", type=['xlsx', 'xlsb'])
+
+
+# For IT
+@st.experimental_memo
+def it():
+    # color function
+    def colour(value):
+        if value > 80:
+            color = 'red'
+        else:
+            color = 'green'
+        return 'color: %s' % color
+
+    df = pd.read_excel(uploaded_f, engine='openpyxl')
+
+    it_df = df.groupby('num_ligne')['nb_appels', 'heure'].sum()
+    it_df['outils'] = "IT"
+    it_df = it_df.sort_values(
+        by='nb_appels',
+        ascending=False
+    )
+    it_df = it_df.style.applymap(colour, subset=['nb_appels'])
+    return it_df
+
+
+if uploaded_f is not None:
+    hvi_dataframe = it()
+    st.dataframe(hvi_dataframe)
+
+    # for it
+    unique_val = len(hvi_dataframe.index.unique())
+    st.write(unique_val)
